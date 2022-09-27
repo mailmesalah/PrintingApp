88f787475b45f1f47ceedfbf12eb47aafe3a55ff
Sely <mailmesalah@gmail.com>
2022-09-27 13:52:43 +0100

    First Commit

---
+++ PrintingApp/PrintingApp/FMain.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FMain))
+        Me.ButtonPrint = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.SuspendLayout()
+        '
+        'ButtonPrint
+        '
+        Me.ButtonPrint.Location = New System.Drawing.Point(85, 103)
+        Me.ButtonPrint.Name = "ButtonPrint"
+        Me.ButtonPrint.Size = New System.Drawing.Size(101, 51)
+        Me.ButtonPrint.TabIndex = 0
+        Me.ButtonPrint.Text = "Print"
+        Me.ButtonPrint.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(2, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(270, 85)
+        Me.TextBox1.TabIndex = 1
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'FMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonPrint)
+        Me.Name = "FMain"
+        Me.Text = "Printing..."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonPrint As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+End Class
---
+++ PrintingApp/PrintingApp/FMain.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Drawing.Printing
+
+Public Class FMain
+    Public doc As PrintDocument = New PrintDocument()
+
+    Private Sub PrintDocument1_PrintPage(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+
+        Static currentChar As Integer
+        Static currentLine As Integer
+        Dim textfont As Font = TextBox1.Font
+        Dim h, w As Integer
+        Dim left, top As Integer
+
+
+
+        Dim printCtl As PreviewPrintController = New PreviewPrintController()
+        'printCtl.UseAntiAlias = UseAntiAlias
+
+        PrintDocument1.PrintController = New PrintControllerWithStatusDialog(printCtl, vbNull)
+
+
+        With PrintDocument1.DefaultPageSettings
+            h = .PaperSize.Height - .Margins.Top - .Margins.Bottom
+            w = .PaperSize.Width - .Margins.Left - .Margins.Right
+            left = PrintDocument1.DefaultPageSettings.Margins.Left
+            top = PrintDocument1.DefaultPageSettings.Margins.Top
+        End With
+
+
+        e.Graphics.DrawRectangle(Pens.Blue, New Rectangle(left, top, w, h))
+        If PrintDocument1.DefaultPageSettings.Landscape Then
+            Dim a As Integer
+            a = h
+            h = w
+            w = a
+        End If
+        Dim lines As Integer = CInt(Math.Round(h / textfont.Height))
+
+        Dim format As StringFormat
+        If Not TextBox1.WordWrap Then
+            format = New StringFormat(StringFormatFlags.NoWrap)
+            format.Trimming = StringTrimming.EllipsisWord
+            Dim i As Integer
+            For i = currentLine To Math.Min(currentLine + lines, TextBox1.Lines.Length - 1)
+                e.Graphics.DrawString(TextBox1.Lines(i), textfont, Brushes.Black, New RectangleF(left, top + textfont.Height * (i - currentLine), w, textfont.Height), format)
+            Next
+            currentLine += lines
+            If currentLine >= TextBox1.Lines.Length Then
+                e.HasMorePages = False
+                currentLine = 0
+            Else
+                e.HasMorePages = True
+            End If
+            Exit Sub
+        End If
+        format = New StringFormat(StringFormatFlags.LineLimit)
+        Dim line, chars As Integer
+        e.Graphics.MeasureString(Mid(TextBox1.Text, currentChar + 1), textfont, New SizeF(w, h), format, chars, line)
+        If currentChar + chars < TextBox1.Text.Length Then
+            If TextBox1.Text.Substring(currentChar + chars, 1) <> " " And TextBox1.Text.Substring(currentChar + chars, 1) <> vbLf Then
+                While chars > 0
+                    TextBox1.Text.Substring(currentChar + chars, 1)
+                    TextBox1.Text.Substring(currentChar + chars, 1)
+                    chars -= 1
+                End While
+                chars += 1
+            End If
+        End If
+        'e.Graphics.DrawString(TextBox1.Text.Substring(currentChar, chars), textfont, Brushes.Black, b, format)
+        currentChar = currentChar + chars
+        If currentChar < TextBox1.Text.Length Then
+            e.HasMorePages = True
+        Else
+            e.HasMorePages = False
+            currentChar = 0
+        End If
+    End Sub
+
+    Private Sub ButtonPrint_Click(sender As Object, e As EventArgs) Handles ButtonPrint.Click
+
+        doc.PrintController = New StandardPrintController()
+
+        AddHandler doc.PrintPage, AddressOf PrintDocumentPrintPage
+
+        doc.Print()
+
+
+    End Sub
+
+    Private Sub PrintDocumentPrintPage(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim drawString As String = "إختبار الرسم"
+        Dim drawBrush As SolidBrush = New SolidBrush(Color.Black)
+        Dim drawFont As Font = New System.Drawing.Font("Arail", 16)
+        Dim format As StringFormat = New StringFormat(StringFormatFlags.DirectionRightToLeft)
+        'format.FormatFlags = StringFormatFlags.DirectionRightToLeft
+        Dim recAtZero As RectangleF = New RectangleF(-100, 10, e.PageBounds.Width, e.PageBounds.Height)
+        e.Graphics.DrawString(drawString, drawFont, drawBrush, New PointF(200, 10), format)
+
+    End Sub
+End Class
